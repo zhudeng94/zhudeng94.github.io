69a5b6aae4ca6a75b881779be63a2052ecaf674e
zhudeng <zhudeng94@gmail.com>
2024-12-24 17:30:50 +0800

    update content

--- publication_generator/pubGen.py
+++ publication_generator/pubGen.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import requests
 
 df = pd.read_excel('files/publications.xlsx', parse_dates=['date'])
 df.sort_values('date', ascending=False,  inplace=True)
+
+js = requests.get('https://cdn.jsdelivr.net/gh/zhudeng94/zhudeng94.github.io@google-scholar-stats/gs_data.json', verify=False).json()
 
 selected_pub = ""
 other_pub = ""
@@ -12,11 +15,15 @@ for index, pub in df.iterrows():
     journal = pub['journal']
     year = pub['date'].year
     gsid = pub['gsid']
+    if gsid in js['publications'].keys():
+        cite_num = js['publications'][gsid]['num_citations']
+    else:
+        cite_num = 0
     doi = pub['doi']
-    almetric = f"<div class='altmetric-embed' data-badge-type='4' data-doi='{doi}'></div>"
-    citation = f'<img src="https://img.shields.io/badge/Citation-300-blue">'
+    almetric = f'<div class="altmetric-embed" data-badge-type="4" data-doi="{doi}"></div>'
+    citation = f'<img src="https://img.shields.io/badge/Citation-{cite_num}-blue">'
 
-    tmp = f"- {author}. [{title}](http://doi.org/{doi}). ***{journal}***. {year}. [<span class='show_paper_citations' data='{gsid}'></span>] {almetric} {citation}  \n"
+    tmp = f"- {author}. [{title}](http://doi.org/{doi}). ***{journal}***. {year}. {citation if cite_num>0 else ''} {almetric}   \n"
 
     if pub['selected'] == 1:
         selected_pub += tmp
