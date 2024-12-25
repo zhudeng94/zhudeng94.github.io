95b0b77dac2ab2818d73074e3916aa9d2cf49000
zhudeng <zhudeng94@gmail.com>
2024-12-25 11:31:16 +0800

    update content

--- _pages/about.md
+++ _pages/about.md
@@ -13,8 +13,9 @@ redirect_from:
 
 {% include_relative includes/pub.md %}
 
+{% include_relative includes/news.md %}
+
 {% include_relative includes/pub_list.md %}
 
-{% include_relative includes/news.md %}
 
 
--- publication_generator/pubGen.py
+++ publication_generator/pubGen.py
@@ -7,7 +7,7 @@ df.sort_values('date', ascending=False,  inplace=True)
 js = requests.get('https://cdn.jsdelivr.net/gh/zhudeng94/zhudeng94.github.io@google-scholar-stats/gs_data.json', verify=False).json()
 
 selected_pub = ""
-other_pub = ""
+all_pub = ""
 
 for index, pub in df.iterrows():
     author = pub['author'].replace('Zhu Deng', '**Zhu Deng**')
@@ -22,24 +22,25 @@ for index, pub in df.iterrows():
     doi = pub['doi']
     altmetric = ''
     # almetric = f'<div class="altmetric-embed" data-badge-type="4" data-doi="{doi}"></div>'
-    citation = f'<img src="https://img.shields.io/badge/citations-{cite_num}-blue">'
+    citation = f'<img src="https://img.shields.io/badge/citations-{cite_num}-white">'
 
     tmp = f"- {author}. [{title}](http://doi.org/{doi}). ***{journal}***. {year}. {citation if cite_num>0 else ''} {altmetric}   \n"
 
     if pub['selected'] == 1:
         selected_pub += tmp
-    else:
-        other_pub += tmp
 
+    all_pub += tmp
 
 
 md = f"""
-## 🌟 SELECTED PUBLICATIONS  
 {selected_pub}
-## 📄 Other Publications  
-{other_pub}
 """
+with open('_pages/includes/pub_selected.md', 'w') as f:
+    f.write(md)
 
-print(md)
+md = f"""
+# 📄 All Publications  
+{all_pub}
+"""
 with open('_pages/includes/pub_list.md', 'w') as f:
     f.write(md)
